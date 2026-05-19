2cdaf0c61f74fc2278e0925c470864b0d0377440
meetrams45 <meetr@fireflyint.com>
2026-05-19 18:34:12 -0400

    update for github deployment

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT for GitHub Pages
+  base: '/rival/',
+
   server: {
     // Allow access from other devices on local network for phone testing
     host: true,
     port: 5173,
   },
+
   build: {
     target: 'es2020',
+
     // Strip console.log in production
     minify: 'esbuild',
   },
